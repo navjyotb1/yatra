5e1bd46d25e055f28ecd5d08986a753ee0e5f4b8
NavjyotGit <navjyotbhosale@gmail.com>
2023-03-06 09:32:41 +0530

    Cab

---
+++ src/main/java/com/example/yatra/Cab.java
@@ -0,0 +1,12 @@
+package com.example.yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class Cab {
+    @GetMapping("/myCab")
+    public String getData(){
+        return "Please book your cab Here";
+    }
+}
